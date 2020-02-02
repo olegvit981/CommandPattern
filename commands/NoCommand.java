c58c752bea4378741f194679bfa751e9a091ea6e
olegvit981 <38032577+olegvit981@users.noreply.github.com>
2020-02-02 16:53:33 +0300

    Create NoCommand.java

---
+++ commands/NoCommand.java
@@ -0,0 +1,15 @@
+package commands;
+
+public class NoCommand implements Command {
+
+    @Override
+    public void execute() {
+        
+    }
+
+    @Override
+    public void undo() {
+        
+    }
+
+}
